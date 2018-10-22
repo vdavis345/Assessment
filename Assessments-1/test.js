1b49ebc9f4759e6f80f71476e29ac0e0848e38eb
COR Students <corstudents@Matts-MBP-109.localdomain>
2018-10-22 12:25:21 -0400

    assessment complete

---
+++ Assessments-1/test.js
@@ -0,0 +1,56 @@
+//Question #1
+// let string = "ysl"
+// let interger = 21
+// let bool = true
+// var pi = 3.14
+
+//Question #2
+// let trueOrFalse = true
+// if(trueOrFalse ){
+//    console.log ("1")
+// }
+// else{
+//     console.log("2")
+// }
+
+//Question #3
+// for(i=0; i <= 100; i++){
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+
+//Question #4
+// let trueOrFalse = true
+// let counter = 1
+// while(trueOrFalse === true){
+//     if(counter <= 20){
+//       console.log("Hello")
+//         counter++
+//     }
+    
+
+
+// }
+
+
+//Question#5
+// let word = "Vincent"
+function name(aName) {
+    let word = "Vincent"
+    if( name === word){
+        console.log("true")
+    }
+    else if(name != word){
+        console.log("false")
+    }
+}
+
+name("Vincent")
+
+
+
+
+
+//Bonus#!
+// Don't Repeat Yourself
